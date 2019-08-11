172622a27ff1e645305bf9e600a6e471b63f9417
adinafometescu <adina.fometescu@gmail.com>
2019-08-11 10:29:40 +0300

    added first version of sns config

---
+++ aws-sns/src/main/java/com/aws/tutorials/sns/Application.java
@@ -0,0 +1,14 @@
+package com.aws.tutorials.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(SNSConfig.class)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ aws-sns/src/main/java/com/aws/tutorials/sns/SNSConfig.java
@@ -0,0 +1,42 @@
+package com.aws.tutorials.sns;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSAsync;
+import com.amazonaws.services.sns.AmazonSNSAsyncClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.amazonaws.services.sqs.model.QueueAttributeName;
+import com.amazonaws.services.sqs.model.SetQueueAttributesRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
+import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.task.AsyncTaskExecutor;
+
+@Configuration
+public class SNSConfig {
+
+    @Value("${cloud.aws.region.static}")
+    protected String awsRegion;
+
+    @Value("${localstack.sqs.url}")
+    protected String localStackSqsUrl;
+
+    @Bean
+    public NotificationMessagingTemplate notificationMessagingTemplate() {
+        return new NotificationMessagingTemplate(amazonLocalSNSAsync());
+    }
+    @Bean
+    @Primary
+    public AmazonSNSAsync amazonLocalSNSAsync() {
+        AmazonSNSAsync amazonSQSAsync = AmazonSNSAsyncClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(localStackSqsUrl, awsRegion))
+                .build();
+        return amazonSQSAsync;
+    }
+}
